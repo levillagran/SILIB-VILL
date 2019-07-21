6907a283793f2fbd0ff7eeab3adcc5bf17b36062
Lenin V. <leninsasos@hotmail.com>
2019-07-20 22:31:01 -0500

    home v1

---
+++ SILIB-VILL/src/main/java/ec/com/silib/SilibVillApplication.java
@@ -0,0 +1,13 @@
+package ec.com.silib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilibVillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilibVillApplication.class, args);
+	}
+
+}
---
+++ SILIB-VILL/src/main/java/ec/com/silib/configuration/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package ec.com.silib.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/*", "/js/*", "/imgs/*", "/vendor/**", "/home").permitAll().anyRequest().authenticated();
+	}
+
+}
